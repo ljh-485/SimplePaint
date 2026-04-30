1118b6740e730a139930f6fdc2b03fd982f92f86
정하 이 <ljhwsad200485@gmail.com>
2026-04-30 09:57:27 +0900

    리드미 추가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblAppName = new Label();
+            groupBox1 = new GroupBox();
+            groupBox3 = new GroupBox();
+            groupBox2 = new GroupBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            cmbColor = new ComboBox();
+            groupBox2.SuspendLayout();
             SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("맑은 고딕", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblAppName.ForeColor = Color.Blue;
+            lblAppName.Location = new Point(40, 40);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(283, 59);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Simple Paint";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(40, 125);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(352, 144);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "도형 선택";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(626, 125);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(294, 144);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "선 두께";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(cmbColor);
+            groupBox2.Location = new Point(398, 125);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(222, 144);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "색 선택";
+            // 
+            // cmbColor
+            // 
+            cmbColor.FormattingEnabled = true;
+            cmbColor.Location = new Point(30, 62);
+            cmbColor.Name = "cmbColor";
+            cmbColor.Size = new Size(156, 40);
+            cmbColor.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1241, 829);
+            ClientSize = new Size(1221, 792);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(lblAppName);
             Name = "Form1";
-            Text = "Form1";
+            Text = "SimplePaint v1.0";
+            groupBox2.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblAppName;
+        private GroupBox groupBox1;
+        private GroupBox groupBox3;
+        private GroupBox groupBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ComboBox cmbColor;
     }
 }
